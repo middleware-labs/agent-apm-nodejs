6ad843b2879900afb03611f450edfb05da82df97
meealmathur23 <meenalmathur23@gmail.com>
2022-08-17 10:48:30 +0530

    export logger functions

--- packages/logger.js
+++ packages/logger.js
@@ -1,14 +1,29 @@
 const FluentClient = require("@fluent-org/logger").FluentClient;
 
-const logger = new FluentClient("tag_prefix", {
+const logger = new FluentClient("melt", {
     socket: {
-        host: "localhost",
+        host: process.env.MELT_LOGGER_ENDPOINT ? process.env.MELT_LOGGER_ENDPOINT : "localhost",
         port: 8006,
-        timeout: 3000, // 3 seconds
+        timeout: 3000,
     }
 });
 
-module.exports.log =  (label,msg) => {
-    logger.emit(label, {record: msg});
+module.exports.error =  (message) => {
+    logger.emit('error', {record:message});
 };
+module.exports.info =  (message) => {
+    logger.emit('info', {record:message});
+};
+module.exports.warn =  (message) => {
+    logger.emit('warn', {record:message});
+};
+module.exports.debug =  (message) => {
+    logger.emit('debug', {record:message});
+};
+
+module.exports.log =  (message) => {
+    logger.emit('log', {record:message});
+};
+
+
 
